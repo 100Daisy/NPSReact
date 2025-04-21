858f107a7dab7edf69dcb4b8946abd7e9acaf420
100Daisy <100Daisy@protonmail.com>
2025-04-22 00:17:10 +0200

    readme: remove screenshots
    Was an additional thing to maintain

--- App.js
+++ App.js
@@ -8,13 +8,15 @@ import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
 import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
 import { useContext, useEffect, useState } from 'react';
 import { trigger } from "react-native-haptic-feedback";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
 } from '@react-navigation/native';
 import DlcsView from './views/dlcs';
 import { SettingsContext, SettingsProvider } from './contexts/SettingsContext';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 export default function App() {
   return (
@@ -26,18 +28,18 @@ export default function App() {
 
 function MainApp() {
   const Tab = createMaterialBottomTabNavigator();
+  const Stack = createStackNavigator();
   const { colorScheme, showDlcsTab } = useContext(SettingsContext);
-  const [showSettings, setShowSettings] = useState(false);
 
   const getTheme = (scheme) => {
-    const colorScheme = useColorScheme();
+    const deviceScheme = useColorScheme();
     const { theme } = useMaterial3Theme();
     
     const { LightTheme, DarkTheme } = adaptNavigationTheme({
       reactNavigationLight: NavigationDefaultTheme,
       reactNavigationDark: NavigationDarkTheme,
     });
-    
+
     const CombinedDefaultTheme = {
       ...MD3LightTheme,
       ...LightTheme,
@@ -45,7 +47,7 @@ function MainApp() {
         ...MD3LightTheme.colors,
         ...theme.light,
       },
-      fonts: MD3LightTheme.fonts, // Ensure fonts are included
+      fonts: MD3LightTheme.fonts,
     };
     const CombinedDarkTheme = {
       ...MD3DarkTheme,
@@ -54,69 +56,80 @@ function MainApp() {
         ...MD3DarkTheme.colors,
         ...theme.dark,
       },
-      fonts: MD3DarkTheme.fonts, // Ensure fonts are included
+      fonts: MD3DarkTheme.fonts,
     };
     if (scheme === 'System') {
-      return colorScheme === 'dark'
-      ? { ...MD3DarkTheme, colors: theme.dark }
-      : { ...MD3LightTheme, colors: theme.light };
+      return deviceScheme === 'dark'
+        ? { ...MD3DarkTheme, colors: theme.dark }
+        : { ...MD3LightTheme, colors: theme.light };
     }
-    return scheme === 'Dark'
-    ? CombinedDarkTheme
-    : CombinedDefaultTheme;
+    return scheme === 'Dark' ? CombinedDarkTheme : CombinedDefaultTheme;
   };
 
   const currentTheme = getTheme(colorScheme);
 
+  const TabNavigator = () => {
+    const navigation = useNavigation();
+  
+    return (
+      <>
+        <Appbar.Header>
+          <Appbar.Content
+            title="NoPayStation"
+            onPress={async () => {
+              const supported = await Linking.canOpenURL('https://nopaystation.com');
+              if (supported) {
+                trigger('effectClick');
+                Linking.openURL('https://nopaystation.com');
+              }
+            }} />
+          <Appbar.Action icon="cog" onPress={() => navigation.navigate('Settings')} />
+        </Appbar.Header>
+        <Tab.Navigator>
+          <Tab.Screen name="Games" component={GamesView} options={{ tabBarIcon: 'gamepad-variant' }} />
+          {showDlcsTab && (
+            <Tab.Screen name="DLC's" component={DlcsView} options={{ tabBarIcon: 'puzzle' }} />
+          )}
+        </Tab.Navigator>
+      </>
+    );
+  };
+  
+
   return (
     <PaperProvider theme={currentTheme}>
-      <NavigationContainer>
+      <NavigationContainer theme={currentTheme}>
         <SafeAreaProvider>
-          {showSettings ? (
-            <>
-            {/* 
-              * Statusbar is required here because we do not have tab navigation
-              * So Appbar.Header won't cover it for us
-              */}
-            <StatusBar
-              barStyle={currentTheme.dark ? 'light-content' : 'dark-content'}
-              backgroundColor="transparent"
-              translucent
+          <Stack.Navigator
+            // screenOptions={{
+            //   animation: 'fade', // inne opcje poniżej 👇
+            // }}
+            >
+            <Stack.Screen
+              name="Home"
+              component={TabNavigator}
+              options={{ headerShown: false }}
             />
-              <Appbar.Header>
-                <Appbar.BackAction onPress={() => {setShowSettings(false)}} />
-                <Appbar.Content title="Settings" />
-              </Appbar.Header>
-              <SettingsView />
-            </>
-          ) : (
-            <>
-              <Appbar.Header>
-                <Appbar.Content
-                  title="NoPayStation"
-                  onPress={async () => {
-                    const supported = await Linking.canOpenURL('https://nopaystation.com');
-                    if (supported) {
-                      trigger('effectClick');
-                      Linking.openURL('https://nopaystation.com');
-                    }
-                  } } />
-                <Appbar.Action icon="cog" onPress={() => setShowSettings(true)} />
-              </Appbar.Header>
-              <Tab.Navigator>
-                  <Tab.Screen
-                    name="Games"
-                    component={GamesView}
-                    options={{ tabBarIcon: 'gamepad-variant' }} />
-                  {showDlcsTab && (
-                    <Tab.Screen
-                      name="DLC's"
-                      component={DlcsView}
-                      options={{ tabBarIcon: 'puzzle' }} />
-                  )}
-              </Tab.Navigator>
-            </>
-          )}
+            <Stack.Screen
+              name="Settings"
+              component={SettingsView}
+              options={{
+                header: ({ navigation }) => (
+                  <>
+                    <StatusBar
+                      barStyle={currentTheme.dark ? 'light-content' : 'dark-content'}
+                      backgroundColor="transparent"
+                      translucent
+                    />
+                    <Appbar.Header>
+                      <Appbar.BackAction onPress={() => navigation.goBack()} />
+                      <Appbar.Content title="Settings" />
+                    </Appbar.Header>
+                  </>
+                ),
+              }}
+            />
+          </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
     </PaperProvider>
--- README.md
+++ README.md
@@ -2,14 +2,12 @@
 [![Android Build](https://github.com/100Daisy/NPSReact/actions/workflows/build.yml/badge.svg?branch=main)](https://github.com/100Daisy/NPSReact/actions/workflows/build.yml)
 
 # NPSReact
-
 Created with React Native, featuring Material You design.
 
 NPSReact allows to browse latest games from [NoPayStation](https://nopaystation.com/).
 
 
 ## Piracy Disclaimer
-
 This application is **not affiliated with, endorsed by, or connected to NoPayStation** or any of its contributors.
 
 This app is designed solely as a **convenient browser tool** to access publicly available metadata. **We do not host, distribute, or promote pirated content of any kind.**
@@ -24,12 +22,7 @@ By using this app, you agree that:
 If you do not agree with these terms, **please do not use this application**.
 
 ## About the code
-
-  
-
 App uses React Native with react-native-paper as a UI framework. This allows the app to comply with latest design standards.
-
-  
 
 - Light/Dark Mode
 
@@ -39,16 +32,7 @@ App uses React Native with react-native-paper as a UI framework. This allows the
 
 - Cutout Aware
 
-  
-
 [react-native-document-picker](https://github.com/rnmods/react-native-document-picker) and [react-native-blob-util](https://github.com/RonRadtke/react-native-blob-util) packages make sure app comply with [Scoped Storage](https://source.android.com/docs/core/storage/scoped)
-
-## Screenshots  
-| ![Test](https://github.com/100Daisy/NPSReact/blob/main/screenshots/dark-1.jpg?raw=true) | ![Test](https://github.com/100Daisy/NPSReact/blob/main/screenshots/dark-2.jpg?raw=true) | ![Test](https://github.com/100Daisy/NPSReact/blob/main/screenshots/dark-3.jpg?raw=true) |
-|--|--|--|
-| ![Test](https://github.com/100Daisy/NPSReact/blob/main/screenshots/white-1.jpg?raw=true) | ![Test](https://github.com/100Daisy/NPSReact/blob/main/screenshots/white-2.jpg?raw=true) |![Test](https://github.com/100Daisy/NPSReact/blob/main/screenshots/white-3.jpg?raw=true) |
-
-
 
 
 
